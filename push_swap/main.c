a0adf509b23bd679fdcb594df3ffd5dfeeb2e595
Filipa Barros <fibarros@c4c3c1.42berlin.de>
2024-01-29 11:42:27 +0100

    continue

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,13 +6,16 @@
 /*   By: fibarros <fibarros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 10:38:05 by fibarros          #+#    #+#             */
-/*   Updated: 2024/01/24 10:58:28 by fibarros         ###   ########.fr       */
+/*   Updated: 2024/01/26 15:45:09 by fibarros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main()
+int	main(int argc, char **argv)
 {
-    
+	s_stack	*a_stack;
+	s_stack	*b_stack;
+	
+	
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fibarros <fibarros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 15:59:27 by fibarros          #+#    #+#             */
-/*   Updated: 2024/01/24 10:47:19 by fibarros         ###   ########.fr       */
+/*   Updated: 2024/01/26 15:43:00 by fibarros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 # define PUSH_SWAP_H
 
 # include "./printf/ft_printf.h"
+
+typedef struct s_stack
+{
+	int				content;
+	int				index;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}	t_stack;
+
 
 
 #endif
