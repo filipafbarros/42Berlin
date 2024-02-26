ed374d4805f7ab5b37c189db9bae78774b364205
Filipa Barros <fibarros@c4a9c1.42berlin.de>
2024-02-26 16:57:25 +0100

    continuing

--- so_long/controls.c
+++ so_long/controls.c
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   controls.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fibarros <fibarros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/14 11:35:42 by fibarros          #+#    #+#             */
-/*   Updated: 2024/02/20 11:05:43 by fibarros         ###   ########.fr       */
+/*   Created: 2024/02/20 11:06:36 by fibarros          #+#    #+#             */
+/*   Updated: 2024/02/20 11:06:44 by fibarros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG.H
-# define SO_LONG.H
-
-# include "minilibx-linux-master/mlx.h"
-# include <stdlib.h>
-
-typedef struct s_game
-{
-	int     fd;
-    int     height;
-    int     width;
-    char    **map;
-    
-};          t_game
-
-
-#endif
+#include "so_long.h"
